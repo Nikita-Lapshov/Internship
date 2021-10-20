fa46a311e9a92eff120fa8fbe959c8bc487508d5
Nikita-Lapshov <nlapschov@mail.ru>
2021-10-21 01:22:09 +0300

    сверстан LP с базовой анимацией

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button--next',
+    prevEl: '.swiper-button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+const modal = document.querySelector('.js-modal')
+        const openBtn = document.querySelector('.js-open-modal')
+        const closeBtn = document.querySelector('.js-close-modal')
+
+        if(modal && openBtn && closeBtn){
+            modal.addEventListener('click', (e)=>{
+                const target = e.target
+                if(target === modal || target === closeBtn){
+                    modal.classList.remove('active')
+                }
+            })
+            openBtn.addEventListener('click', ()=>{
+                modal.classList.add('active')
+            })
+
+        }
